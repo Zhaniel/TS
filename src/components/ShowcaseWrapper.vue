14f842a0af2a7377ca11a25582ce0abf0e2f3772
aleksanderwalczuk <ohleg@windowslive.com>
2023-09-04 09:05:58 +0200

    render swiper modules

--- src/components/ShowcaseWrapper.vue
+++ src/components/ShowcaseWrapper.vue
@@ -1,31 +1,26 @@
 <script setup lang="ts">
 import { onMounted, ref } from "vue";
 import Modal from "./Modal.vue";
-import Swiper from "./Swiper.vue";
+import Slider from "./Slider.vue";
 
 const open = ref(false);
 const props = defineProps<{
   slide: {
-    id: string;
-    collection: string;
-    data: {
-      featured?: number;
-      url: string;
-      title: string;
-      image: string;
-    };
+    sources: string[];
+    title: string;
+    url: string;
+    featured: number;
   };
 }>();
 const mounted = ref(false);
 onMounted(() => {
   mounted.value = true;
-  console.log(props.slide);
 });
 </script>
 <template>
   <div class="absolute inset-0" @click="open = true"></div>
   <Modal v-if="mounted" size="xl" :open="open" @close="open = false"
-    >{{ slide.data.title }} is open
-    <Swiper :sources="[slide.data.url, slide.data.url]" />
+    >{{ slide.title }}
+    <Slider :sources="slide.sources" />
   </Modal>
 </template>
